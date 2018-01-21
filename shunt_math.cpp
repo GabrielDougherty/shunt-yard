5963a1f521b545ec95c2515db0a596ca028f0e8d
Gabriel Dougherty <gabrieljosephdougherty@gmail.com>
2018-01-20 23:09:47 -0500

    Adds comments, fixes bug
    Takes string as input now so it can handle multiple-digit
    numbers (duh)

--- shunt_math.cpp
+++ shunt_math.cpp
@@ -1,26 +1,34 @@
 // Converts infix arithmetic operations to postfix
+// Implements the algorithm from this description:
+// http://www.oxfordmathcenter.com/drupal7/node/628
 
 #include <iostream>
 #include <stack>
 
 using namespace std;
+
+// OpInfo describes an operator's character and priority.
+// zero-priority operators are + and -.
 struct OpInfo {
 	char opChar;
 	int priority = 0;
 };
 
-class ShuntMath {
+// ShuntMather is a dumb class. Just make one and call ProcessInput().
+// Then the user enters an algebraic expression followed by " q" and hits Enter.
+class ShuntMather {
 public:
+	// process user input
 	void ProcessInput() {
-		char c;
-		for (cin >> c; c != 'q'; cin >> c) {
-			if (IsValidOp(c)) {
+		string input;
+		for (cin >> input; input != "q"; cin >> input) {
+			if (IsValidOp(input.at(0))) {
 				OpInfo op;
-				op.opChar = c;
+				op.opChar = input.at(0);
 				DetPriority(op);
 				ProcessOp(op);
 			} else {
-				cout << c << " ";
+				cout << input << " ";
 			}
 		}
 		while (!operators.empty()) {
@@ -30,14 +38,23 @@ public:
 		cout << endl;
 	}
 private:
+	// Priority of *,/ is 1. Priority of ^ is 2. This is due to the order of
+	// arithmetic operations.
 	void DetPriority(OpInfo& op) {
 		if (op.opChar == '*' ||
 			op.opChar == '/')
 			op.priority = 1;
+		else if (op.opChar == '^')
+			op.priority = 2;
+		// otherwise, the struct constructor took care of priority = 0
 	}
+
 	bool IsValidOp(char c) {
 		return opChars.find(c) != string::npos;
 	}
+
+	// This handles comparing priority and printing the operator along the way,
+	// if necessary.
 	void ProcessOp(OpInfo op) {
 		if (!operators.empty()) {
 			if (op.priority <= operators.top().priority) {
@@ -47,13 +64,14 @@ private:
 		}
 		operators.push(op);
 	}
+	
 	stack<OpInfo> operators;
 	static const string opChars;
 };
 
-const string ShuntMath::opChars = "+-*/";
+const string ShuntMather::opChars = "+-*/^";
 
 int main() {
-	ShuntMath mather;
+	ShuntMather mather;
 	mather.ProcessInput();
 }
